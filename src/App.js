7967bffc65eb7bb64b2db41fc90118e260871ea6
jagadeshsubash <subash.jagadesh@gmail.com>
2022-03-22 23:59:15 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Hello eeeee StackBlitz!</h1>
-      <img  src = "basic-image.png"">
+      <img  src = "basic-image.png">
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
